d93f7c927397ad320e8c139b86f64fcde9c73a2f
parth-mango <59818540+parth-mango@users.noreply.github.com>
2022-03-28 19:58:49 +0530

    added to repo

--- utils/datasets.py
+++ utils/datasets.py
@@ -7,8 +7,12 @@ import torch
 from torch.utils import data
 from torch.utils.data import Dataset
 
+# path= os.path.exists('data/small_coco/smallcoco.txt')
+
 class TensorDataset():
+#   print(os.path.exists('data/small_coco/smallcoco.txt'))
   def __init__(self, path, img_size_width= 352, img_size_height= 352,imgaug= False):
+    
     assert os.path.exists(path)," %s The file path is wrong or does not exist" % path
 
     self.path= path
@@ -22,5 +26,19 @@ class TensorDataset():
     with open(self.path, 'r') as f:
       for line in f.readlines():
         data_path= line.strip()
+        
         if os.path.exists(data_path):
           img_type= data_path.split(".")[-1]
+          if img_type not in self.img_formats:
+            raise Exception("img type error:%s" % img_type)
+          else:
+            self.data_list.append(data_path)
+        else: 
+          raise Exception("%s does not exist" % data_path)
+
+  def __getitem__(self, index):
+    img_path= self.data_list[index]
+    print(img_path)
+    # label_path= img_path.split(".")[0] + ".txt"          
+
+          
