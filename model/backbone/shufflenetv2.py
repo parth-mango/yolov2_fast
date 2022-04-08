093b9cc615f2ec0b34e015224d257f6eed8a9e85
parth-mango <59818540+parth-mango@users.noreply.github.com>
2022-04-09 00:51:47 +0530

    added to repo

---
+++ model/backbone/shufflenetv2.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+class ShuffleV2Block(nn.Module):
+  def __init__(self, inp, oup, mid_channels, *, ksize, stride):
+    super(ShuffleV2Block, self).__init__()
+    self.stride= stride
+    assert stride in [1, 2]
+
+    self.mid_channels= mid_channels
+    self,ksize= ksize
+    pad= ksize // 2
+    self.pad= pad
+    self.inp= inp
+    outputs= oup - inp
+
+
+    branch_main = [
+                  #pw
+                  nn.Conv2d(inp, mid_channels, 1, 1, 0, bias= False),
+                  nn.BatchNorm2d(mid_channels),
+                  nn.ReLU(inplace= True),
+
+                  #dw
+                  nn.Conv2d(mid_channels, mid_channels, ksize, stride, pad, groups= mid_channels, bias= False),
+                  nn.BatchNorm2d(mid_channels),
+                  
+                  #pw-linear
+                  nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias= False),
+                  nn.BatchNorm2d(outputs),
+                  nn.ReLU(inplace= True),
+              
+                  ]
+    self.branch_main= nn.Sequential(*branch_main)
+
+    if stride == 2:
+      branch_proj = [
+                  #dw
+                  nn.Conv2d(inp, inp, ksize, stride, pad, groups= inp, bias= False),
+                  nn.BatchNorm2d(inp),
+                  #pw-linear              
+                  nn.Conv2d(inp, inp, 1, 1, 0, bias= False),
+                  nn.BatchNorm2d(inp),
+                  nn.ReLU(inplace= True),
+          
+                    ]  
+      self.branch_proj= nn.Sequential(*branch_proj)
+    
+    else:
+      self.branch_proj= None
+
