fdebb9eb9cc2d8311dfc5afe868239696637cfee
parth-mango <59818540+parth-mango@users.noreply.github.com>
2022-06-05 01:28:22 +0530

    update

--- utils/loss.py
+++ utils/loss.py
@@ -60,7 +60,13 @@ def build_target(preds, targets, cfg, device):
       #offsets
       gxy= t[:, 2:4] # grid xy
       gxi = gain[[2, 3]] - gxy  # inverse - from the other side of origin
-         
+
+      j,k = ((gxy % 1. < g ) & (gxy > 1.)).T # Rounding off?
+      l, m= ((gxi % 1. < g) & (gxi >1.)).T
+      j= torch.stack((torch.ones_like(j), j, k, l, m)) # stacking "All true value with dim = dim(j)" along with j, k, l,m"
+      t= t.repeat((5, 1, 1 ))[j] # We keep only those targets which have decimal part less than 0.5 either for w or h
+    
+
 def smooth_BCE(eps= 0.1):
   #return positive, negative label smoothing BCE targets
   return 1.0 - 0.5 * eps, 0.5 * eps
